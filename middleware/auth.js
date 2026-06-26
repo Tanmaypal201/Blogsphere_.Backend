85dfbdfaf04512c245e04c3a15373d3110308d15
Tanmaypal201 <tanmaypal5237@gmail.com>
2026-06-26 12:49:44 +0530

    cookie

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,8 @@ const { getuser } = require('../server/auth');
 
 const checkauthentication = (req, res, next) => {
     const token = req.cookies.token;
+    console.log("Cookies:", req.cookies);
+    console.log("Token:", req.cookies.token);
     if (!token) {
         req.user = null;
         return res.status(401).json({ message: "Unauthorized" });
