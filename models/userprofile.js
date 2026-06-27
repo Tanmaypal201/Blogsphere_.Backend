6b0b4f6d8ef4cee2c89b6cc6eaa1190f6bb10579
Tanmaypal201 <tanmaypal5237@gmail.com>
2026-06-27 13:35:29 +0530

    maa !!!

--- models/userprofile.js
+++ models/userprofile.js
@@ -1,37 +1,37 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const userProfileSchema=new mongoose.Schema({
-    userId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-        unique: true
-    },
-    username:{
-        type: String,
-        ref: "User",
-        required: true,
-        unique: true
-    },
-    fullName: { type: String, default: "" },
-    bio: { type: String, default: "" },
-    profilePicture: {
-        url: { type: String, default: "" },
-        publicId: { type: String, default: "" },
-        fileName: { type: String, default: "" },
-        fileSize: { type: Number, default: null },
-        mimeType: { type: String, default: "" },
-        resourceType: { type: String, default: "" }
-    },
-    interests: { type: [String], default: [] },
-    followers: [{
-        userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-        username: String
-    }],
-    following: [{
-        userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-        username: String
-    }]
+const userProfileSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    ref: "User",
+    required: true,
+    unique: true
+  },
+  fullName: { type: String, default: "" },
+  bio: { type: String, default: "" },
+  profilePicture: {
+    url: { type: String, default: "" },
+    publicId: { type: String, default: "" },
+    fileName: { type: String, default: "" },
+    fileSize: { type: Number, default: null },
+    mimeType: { type: String, default: "" },
+    resourceType: { type: String, default: "" }
+  },
+  interests: { type: [String], default: [] },
+  followers: [{
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    username: String
+  }],
+  following: [{
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    username: String
+  }]
 });
 
 userProfileSchema.set("toJSON", {
@@ -41,11 +41,9 @@ userProfileSchema.set("toJSON", {
     if (ret.profilePicture) {
       const url = typeof ret.profilePicture === "object" ? ret.profilePicture.url : ret.profilePicture;
       if (url && (url.startsWith("http://") || url.startsWith("https://"))) {
+        ret.profilePicture = `/uploads/${url}`;
+      } else {
         ret.profilePicture = url;
-      } else if (url && url.startsWith("/")) {
-        ret.profilePicture = `${process.env.NEXT_PUBLIC_API_URL}${url}`;
-      } else if (url) {
-        ret.profilePicture = `${process.env.NEXT_PUBLIC_API_URL}/${url}`;
       }
     }
     return ret;
@@ -68,6 +66,6 @@ userProfileSchema.set("toObject", {
   }
 });
 
-const UserProfile=mongoose.model("Userprofile",userProfileSchema);
+const UserProfile = mongoose.model("Userprofile", userProfileSchema);
 
-module.exports=UserProfile;
+module.exports = UserProfile;
